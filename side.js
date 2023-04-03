51c8addc584171f60fe6b3f4e0d995580b128d91
Sushitaro0111 <Hide.o.k.310@gmail.com>
2023-04-04 02:23:38 +0900

    Sideクラスの修正 　・コメントの修正 　・辺がx軸に対して垂直だった場合、傾きと切片がInfinityとなる関係で 　　交差判定が期待通りに処理されない問題を修正

--- side.js
+++ side.js
@@ -7,11 +7,11 @@ class Side {
    */
   constructor(vertices) {
 
-    // 線分を構成する2つの頂点
+    // 線分を構成する2つの頂点情報
     this.vertices = vertices;
-
-    // 線分の一次関数式(交差判定しやすいようにy-ax-bの形式で持っておく) 
+    // 辺の傾き
     this.slope = this.getSlope(vertices);
+    // 辺の切片
     this.slice = this.getSlice(vertices[0], this.slope);
   }
   
@@ -22,12 +22,18 @@ class Side {
    * targetSide = [ [0, 0], [3, 3] ];
    */
   isCross(targetSide) {
-    // 位置情報の線分を通る直線が対象の線分を構成する二頂点と交差し、
-    // かつ対象の線分を通る直線が位置情報を構成する二頂点と交差するとき、二つの線分は交差する
+    // 位置情報の二頂点を通る直線が辺の線分を構成する二頂点と交差し、
+    // かつ辺の線分を通る直線が位置情報を構成する二頂点と交差するとき、二つの線分は交差する
     let cnt = 0;
     targetSide.vertices.forEach(vertice => {
-      // y-ax-bにtargetSideの頂点情報を代入し、0より大きければカウントアップ、0より小さければカウントダウン
-      vertice[1] - ( this.slope * vertice[0] ) - this.slice > 0 ? cnt++ : cnt--; 
+      if (!isFinite(this.slope) || !isFinite(this.slice)) {
+        // 傾きもしくは切片のいずれかがInfiniteだった場合、対象のx座標が辺を構成するx座標よりも大きければカウントアップ、
+        // 対象のx座標が辺を構成するx座標よりも小さければカウントダウン
+        vertice[0] - this.vertices[0][0] > 0 ? cnt++ : cnt--;
+      } else {
+        // y-ax-bにtargetSideの頂点情報を代入し、0より大きければカウントアップ、0より小さければカウントダウン
+        vertice[1] - ( this.slope * vertice[0] ) - this.slice > 0 ? cnt++ : cnt--; 
+      }
     });
     // 最終的にカウントが0ならば交差している
     return cnt === 0 ? true : false;
